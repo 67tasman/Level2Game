528878ccefa43c1b3c82bf86e37fc413892cf6f7
67tasman <league@WTS14.attlocal.net>
2018-05-25 17:01:03 -0700

    game2

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,15 @@
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Game();
+
 	}
-Game(){
-	View v = new View();
-	v.gameScreen();
-}
+
+	Game() {
+		View v = new View();
+		v.home();
+		// v.playerSelect();
+		// v.gameScreen();
+	}
 }
--- src/View.java
+++ src/View.java
@@ -1,18 +1,54 @@
 import java.awt.Color;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class View {
 
-	void gameScreen(){
+	void home() {
 		JFrame frame = new JFrame();
-		frame.setSize(1000,1000);
+		frame.setSize(1000, 1000);
 		JPanel panel = new JPanel();
 		frame.add(panel);
-		panel.setBackground(Color.YELLOW);
+		panel.setBackground(Color.WHITE);
 		panel.setOpaque(true);
 		frame.setVisible(true);
+		JButton button = new JButton();
+		JButton button2 = new JButton();
+		panel.add(button);
+
+		try {
+			ImageIcon icon = new ImageIcon(ImageIO.read(new View().getClass().getResourceAsStream("start.png")));
+			button.setIcon(icon);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
+
+	}
+
+	void playerSelect() {
+		JFrame frame = new JFrame();
+		frame.setSize(1000, 1000);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		panel.setBackground(Color.LIGHT_GRAY);
+		panel.setOpaque(true);
+		frame.setVisible(true);
+	}
+
+	void gameScreen() {
+		JFrame frame = new JFrame();
+		frame.setSize(1000, 1000);
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		panel.setBackground(Color.GREEN);
+		panel.setOpaque(true);
+		frame.setVisible(true);
+	}
+
 }
