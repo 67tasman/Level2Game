52d66a8c8fd454d07cfa13031d84e980fe3d0f8a
67tasman <league@iMac-7.attlocal.net>
2018-09-01 16:01:51 -0700

    9/1/2018

--- src/View.java
+++ src/View.java
@@ -1,7 +1,11 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -27,23 +31,46 @@ public class View implements ActionListener {
 	// GameScreen gameFrame = new GameScreen();
 	void home() {
 		homeFrame.setSize(1000, 1000);
+		homeFrame.setLayout(new BorderLayout());
 		JPanel panel = new JPanel();
-		homeFrame.add(panel);
-		panel.setBackground(Color.WHITE);
-		panel.setOpaque(true);
+		JLabel buttonLabel = new JLabel();
+		JLabel label1 = new JLabel();
+		JLabel label2 = new JLabel();
+		JLabel sideLabel = new JLabel(new ImageIcon("sparkles.png"));
+		JLabel title = new JLabel("<html><span style='font-size:20px'>"+"Fun Game"+ "</span></html>");
+		JPanel panel1 = new JPanel();
+		JPanel sidePanel = new JPanel();
+		JPanel titlePanel = new JPanel();
+		label1.setText("Use the arrow keys or W,A,S,D to move.");
+		label2.setText("Escape the Castle");
+		//title.setText("Fun Game");
+		//sideLabel.add(new ImageIcon("sparkles.png"));
+		//panel.setBackground(Color.ORANGE);
+		//panel.setOpaque(true);
+		//panel.add(startButton);
 		homeFrame.setVisible(true);
 		startButton.addActionListener(this);
-		// JButton button2 = new JButton();
-		panel.setLayout(null);
-		panel.add(startButton);
+		startButton.setBackground(Color.YELLOW);
+		startButton.setOpaque(true);
+		startButton.setBorderPainted(false);
+		panel1.setLayout(new GridLayout(2,1));
+		panel1.add(label1);
+		panel1.add(label2);
+		sidePanel.add(sideLabel);
+		titlePanel.add(title);
+		homeFrame.add(startButton, BorderLayout.SOUTH);
+		homeFrame.add(panel1, BorderLayout.CENTER);
+		homeFrame.add(titlePanel, BorderLayout.NORTH);
+		homeFrame.add(sidePanel, BorderLayout.EAST);
+		homeFrame.add(sidePanel, BorderLayout.WEST);
 		try {
 			ImageIcon icon = new ImageIcon(ImageIO.read(this.getClass().getResourceAsStream("start.png")));
-			startButton.setIcon(icon);
+		startButton.setIcon(icon);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		startButton.setBounds(275, 350, 450, 150);
+
 
 	}
 
